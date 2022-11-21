ee6ad73b2eb925db228b83f5d38a7ee67540e7e9
Korurg <ilea.golushev@gmail.com>
2022-11-21 14:03:08 +0300

    init

---
+++ korurg-chat/src/main/java/korurg/korurgchat/KorurgChatApplication.java
@@ -0,0 +1,12 @@
+package korurg.korurgchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KorurgChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KorurgChatApplication.class, args);
+    }
+}
---
+++ twitch-api/src/main/java/korurg/twitch/TestClass.java
@@ -0,0 +1,4 @@
+package korurg.twitch;
+
+public class TestClass {
+}
